44c2b4e01c64f2664dd875dca3531d0a7da199d1
tanu <24976766+tanusgit@users.noreply.github.com>
2020-08-22 10:58:06 -0700

    assignment 2 added person, emplyee class

--- Assignment2/src/cs151/Employee.java
+++ Assignment2/src/cs151/Employee.java
@@ -3,20 +3,55 @@ package cs151;
 public class Employee extends Person{
 	int employeeId;
 	String employeeStatus;
-	int payAmount;
+	double basePay;
+	double salary;
+	int unit;
+	
+	//Joe Smith, a contractor, pay is $60/hr, should get paid or working 30 hours,
+	public double calculatePay(int unit) {
+		this.unit = unit;
+		if(employeeStatus.equals("fulltime")) {
+			salary = basePay * 1/52 * unit;
+		}
+		else if(employeeStatus.equals("parttime")) {
+			salary = basePay * unit;
+			
+		}
+		else if(employeeStatus.equals("contractor")) {
+			salary = basePay * unit;
+		}
+		//String strSalary = String.format("%.2f", salary);  
+		//System.out.println("salary of the employee is $" + strSalary);
+		return salary;
+		
+	}
+	
+	
 	
 	public Employee(String firstname, String lastname, int age, int socialSecurityNumber, String address, String gender,
-			double weight, int employeeId, String employeeStatus, int payAmount) {
+			double weight, int employeeId, String employeeStatus, double basePay) {
 		super(firstname, lastname, age, socialSecurityNumber, address, gender, weight);
 		this.employeeId = employeeId;
 		this.employeeStatus = employeeStatus;
-		this.payAmount = payAmount;
+		this.basePay = basePay;
 	}
-	
+
+
+
 	public void introduce() {
 		String res;
-		res = firstname + " " + lastname + " " + age + " " + socialSecurityNumber + " " + address + "" + gender + " "
-				+ weight + " " + employeeId + " " + employeeStatus + " " + payAmount;
+		String basePays = "";
+		
+		if(basePay < 999) {
+			basePays = basePay + "/hr";
+		}
+		else if(basePay > 20000) {
+			basePays = basePay + "/yr";
+		}
+		res = firstname + " " + lastname  + ", a " + employeeStatus + " employee, "
+		+ "pay is $"  + basePays + ", working " + unit + " hours ,"+ " age is " + age + ", social security number is " + 
+		socialSecurityNumber + ", address is " + address + ", gender is " + gender +
+		", weight is " + weight + ", employee id is " + employeeId ;
 		System.out.println(res);
 	}
 	
---
+++ Assignment2/src/cs151/EmployeeTest.java
@@ -0,0 +1,14 @@
+package cs151;
+
+public class EmployeeTest {
+public static void main(String[] args) {
+	Employee emp1 = new Employee("Joe", "Smith", 45, 7894561, "San Jose", "Male", 123.2, 1, "parttime", 120.0);
+	double salary  = emp1.calculatePay(2);
+	String strSalary = String.format("%.2f", salary);  
+	System.out.println("the employee should get paid $" + strSalary);
+	//Joe Smith, a contractor, pay is $60/hr, 
+	//should get paid or working 30 hours, other details are up to you
+	emp1.introduce();
+	
+}
+}
