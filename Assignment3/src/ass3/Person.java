2f7f611e2fb298c12ae8b4e7d6bd376edbf18161
tanu <24976766+tanusgit@users.noreply.github.com>
2020-09-06 22:04:58 -0700

    updated address and person classes

--- Assignment3/src/ass3/Person.java
+++ Assignment3/src/ass3/Person.java
@@ -1,5 +1,57 @@
 package ass3;
 
 public class Person {
+	private String firstName;
+	private String lastName;
+	private int age;
+	private String ssn;
+	private Address address;
+
+	public Person(String firstName, String lastName, Address address) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
 
 }
