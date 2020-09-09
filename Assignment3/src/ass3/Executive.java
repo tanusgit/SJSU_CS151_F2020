ce1cec6ef746372a8a779281853f9198622bd0f6
tanu <24976766+tanusgit@users.noreply.github.com>
2020-09-09 15:14:41 -0700

    executive

--- Assignment3/src/ass3/Executive.java
+++ Assignment3/src/ass3/Executive.java
@@ -3,4 +3,11 @@ package ass3;
 public class Executive extends Employee {
 	private Double yearlyBonus;
 	private Double yearlyCompensation;
+	public static void main(String[] args) {
+		Executive.pin();
+	}
+	
+	public static void pin() {
+		System.out.println("nim is");
+	}
 }
