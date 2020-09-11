18af307e7e38b170e67f9cae2d099790e15f955a
tanu <24976766+tanusgit@users.noreply.github.com>
2020-09-10 19:26:21 -0700

    update partTimeHourly class

--- Assignment3/src/ass3/PartTimeHourly.java
+++ Assignment3/src/ass3/PartTimeHourly.java
@@ -1,5 +1,31 @@
 package ass3;
 
 public class PartTimeHourly {
-
+	private Double baseHourlyPay;
+	public PartTimeHourly() {
+		System.out.println(100.0); 
+		}
+	public PartTimeHourly( String firstName, String lastName, Address adress, Double pay)
+	{
+	}
+	public Double getHourlyPay() {
+	return 0.0;
+	}
+	
+	public void setHourlyPay(Double pay) {
+		
+	}
+	
+	public String toString() {
+		return "";
+	}
+	
+	public void introduce(boolean displaySSN) {
+		
+	}
+	
+	public void computePay(int numHrs){
+		
+	}
 }
+
