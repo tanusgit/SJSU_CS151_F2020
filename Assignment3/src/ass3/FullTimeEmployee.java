2035a46465d0fb0b2e08986986e848ca68ab3545
tanu <24976766+tanusgit@users.noreply.github.com>
2020-09-13 19:47:24 -0700

    updated fulltimeemployee

--- Assignment3/src/ass3/FullTimeEmployee.java
+++ Assignment3/src/ass3/FullTimeEmployee.java
@@ -1,6 +1,21 @@
 package ass3;
 
-public class FullTimeEmployee {
-String firstname;
-String lastname;
+public abstract class FullTimeEmployee extends Employee{
+	private Double basePay;
+	
+	public FullTimeEmployee() {
+		System.out.println(basePay);
+	}
+
+	public FullTimeEmployee(String firstName, String lastName, Address address,
+	int id, Double basePay) {
+		super(firstName, lastName, address, id);
+		this.basePay = basePay;
+	}
+	public Double getBasePay() {
+		return basePay;
+	}
+	public void setBasePay(Double pay) {
+		basePay = pay;
+	}
 }
