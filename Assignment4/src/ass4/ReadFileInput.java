a97d647e2fa2c0e9c82adf42a06dbb08b6364976
tanu <24976766+tanusgit@users.noreply.github.com>
2020-09-20 18:44:27 -0700

    added assignment4

---
+++ Assignment4/src/ass4/ReadFileInput.java
@@ -0,0 +1,14 @@
+package ass4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ReadFileInput {
+	public static void main(String[] args) throws FileNotFoundException {
+		File file = new File("/home/td/Desktop/SJSU/CS151/SJSU_CS151_F2020/Assignment4/src/ass4/quote.txt");
+		FileReader r = new FileReader(file);
+		
+
+	}
+}
