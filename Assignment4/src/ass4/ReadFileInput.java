74d941af81e4bab6cdcf216668f600d70576b4b0
tanu <24976766+tanusgit@users.noreply.github.com>
2020-09-21 10:04:50 -0700

    solved 2nd question

--- Assignment4/src/ass4/ReadFileInput.java
+++ Assignment4/src/ass4/ReadFileInput.java
@@ -3,12 +3,15 @@ package ass4;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class ReadFileInput {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		File file = new File("/home/td/Desktop/SJSU/CS151/SJSU_CS151_F2020/Assignment4/src/ass4/quote.txt");
 		FileReader r = new FileReader(file);
-		
-
+		int line;
+		while ((line = r.read()) != -1)
+			System.out.print((char) line);
 	}
+
 }
