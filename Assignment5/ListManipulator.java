edb49c76c6c4f68593e16d268ae3c43d260f4da5
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-09-24 17:49:40 -0700

    Add files via upload

--- Assignment5/ListManipulator.java
+++ Assignment5/ListManipulator.java
@@ -3,31 +3,72 @@ package a5;
 import java.util.ArrayList;
 
 public class ListManipulator {
-	//Accepts an array list of integers and sorts it in ascending or descending order.
-	//Make sure to account for cases with duplicate values. The method returns the 
-	//sorted list. 
-	ArrayList<Integer> sort(ArrayList<Integer> myLst, Boolean ascending){
-		ArrayList<Integer> newList = new ArrayList<Integer>();
-		if(ascending == true) {
-			for(int n : myLst) {
-				for(int i = 0; i < myLst.size()-2; i++) {
-					for(int j = i +1 ; j < myLst.size()-1; j++) {
-						if(myLst.get(i) > myLst.get(j)) {
-							int temp = myLst.get(i);
-							myLst.add(i, myLst.get(j));
-							myLst.add(j, temp);		
-						}
+	// Accepts an array list of integers and sorts it in ascending or descending
+	// order.
+	// Make sure to account for cases with duplicate values. The method returns the
+	// sorted list.
+	ArrayList<Integer> sort(ArrayList<Integer> myLst, Boolean ascending) {
+		if (ascending == true) {
+			for (int i = 0; i < myLst.size() - 2; i++) {
+				for (int j = i + 1; j < myLst.size() - 1; j++) {
+					if (myLst.get(i) > myLst.get(j)) {
+						int temp = myLst.get(i);
+						myLst.set(i, myLst.get(j));
+						myLst.set(j, temp);
 					}
 				}
-			}
-		}	
+			}	
+		}
+		else if (ascending == false) {
+			for (int i = 0; i < myLst.size() - 2; i++) {
+				for (int j = i + 1; j < myLst.size() - 1; j++) {
+					if (myLst.get(i) < myLst.get(j)) {
+						int temp = myLst.get(i);
+						myLst.set(i, myLst.get(j));
+						myLst.set(j, temp);
+					}
+				}
+			}		
+		}
 		return myLst;
 	}
 	
-	private void Swap(int temp, int temp2) {
-		int a = temp;
-		temp = temp2;
-		temp2 = a;	
+	ArrayList<Integer> swapLargestSmallest(ArrayList<Integer> myLst){
+		int i;
+		Integer largest = 0;
+		Integer smallest = myLst.get(0);
+		Integer indexOflargest;
+		Integer indexOfsmallest;
+		
+		for (i = 0; i < myLst.size() - 1; i++) {
+			System.out.println( i);
+			if (myLst.get(i) > largest) {
+				largest = myLst.get(i);
+				System.out.println(" just before index" + i + " largest" + largest + " "
+						+ myLst.get(i));
+			
+			}
+		}
+		
+		return myLst;
+		
+	}
+	
+	public static int[] swap(int[] array) {
+	    int minIndex = 0, maxIndex = 0;
+	    for (int i = 1; i < array.length; ++i) {
+	        if (array[i] < array[minIndex])
+	            minIndex = i;
+	        if (array[i] > array[maxIndex])
+	            maxIndex = i;
+	    }
+	    int t;
+	    if (maxIndex != minIndex) {
+	        t = array[minIndex];
+	        array[minIndex] = array[maxIndex];
+	        array[maxIndex] = t;
+	    }
+	    return array;
 	}
 
 	public static void main(String[] args) {
@@ -41,10 +82,16 @@ public class ListManipulator {
 		array.add(1);
 		array.add(8);
 		array.add(9);
-		/*ListManipulator m = new ListManipulator();
-		result = m.sort(array, true);
-		System.out.println(result);*/
-		System.out.println(123);
+		ListManipulator m = new ListManipulator();
+		result = m.sort(array, false);
+		System.out.println(result);
+		//m.swapLargestSmallest(array);
+		int arr[] = {1, 5, 4, 6, 8};
+		int a[] = swap(arr);
+		for(int l : a) {
+			System.out.println(l);
+		}
+		
 		
 	}
 }
