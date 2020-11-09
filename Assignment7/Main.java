30657be9d6c42eff61a23ab87547eeec537cd7c3
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-11-09 09:58:07 -0800

    Add files via upload

---
+++ Assignment7/Main.java
@@ -0,0 +1,266 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.embed.swing.JFXPanel;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.event.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+      /*  Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();*/
+        String currentOperation = "";
+        String finaloutput = "";
+        double firstOperand = 0.0;
+        Panel displayPanel = new Panel();
+
+
+        TextField outputfield = new TextField("0");
+        Button b0 = new Button("0");
+        Button b1 = new Button("1");
+        Button b2 = new Button("2");
+        Button b3 = new Button("3");
+        Button b4 = new Button("4");
+        Button b5 = new Button("5");
+        Button b6 = new Button("6");
+        Button b7 = new Button("7");
+        Button b8 = new Button("8");
+        Button b9 = new Button("9");
+
+        Button b11 = new Button("+");
+        Button b12 = new Button("-");
+        Button b13 = new Button("*");
+        Button b14 = new Button("/");
+        Button b15 = new Button("=");
+        Button ac = new Button("AC");
+
+        GridPane pane = new GridPane();
+        Button b55 = new Button("9");
+        pane.add(b55, 0, 0);
+
+
+        VBox root = new VBox();
+
+        root.setSpacing(10);
+        root.setPadding(new Insets(15, 10, 12, 15));
+        root.setStyle("-fx-background-color: #CC2AFE;");
+        ObservableList<Button> list = FXCollections.observableArrayList();
+        root.getChildren().add(outputfield);
+        root.getChildren().addAll(b1, b2, b3, b4, b5, b6, b7, b8, b9, b0);
+
+
+        HBox hbox = new HBox();
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(b11, b12, b13, b14, b15, ac);
+        hbox.getChildren().addAll(vBox, root);
+
+
+        EventHandler<MouseEvent> mouse = new EventHandler<MouseEvent>() {
+            String output = "";
+
+            public void handle(MouseEvent event) {
+                Object source = (Object) event.getSource();
+                if ((Button) source == b1) {
+                    output += "1";
+                } else if ((Button) source == b0) {
+                    output += "0";
+                } else if ((Button) source == b2) {
+                    output += "2";
+                } else if ((Button) source == b2) {
+                    output += "2";
+                } else if ((Button) source == b3) {
+                    output += "3";
+                } else if ((Button) source == b4) {
+                    output += "4";
+                } else if ((Button) source == b5) {
+                    output += "5";
+                } else if ((Button) source == b6) {
+                    output += "6";
+                } else if ((Button) source == b7) {
+                    output += "7";
+                } else if ((Button) source == b8) {
+                    output += "8";
+                } else if ((Button) source == b9) {
+                    output += "9";
+                } else if ((Button) source == b11) {
+                    output += "+";
+                } else if ((Button) source == b12) {
+                    output += "-";
+                } else if ((Button) source == b13) {
+                    output += "*";
+                } else if ((Button) source == b14) {
+                    output += "/";
+                } else if ((Button) source == b15) {
+                    String o = cal(output);
+                    output = o;
+                }
+                else if ((Button) source == ac) {
+                    output = "";
+                }
+                outputfield.setText(output);
+            }
+        };
+
+        ac.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b0.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b1.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b2.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b3.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b4.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b5.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b6.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b7.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b8.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b9.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b11.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b12.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b13.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b14.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+        b15.addEventHandler(MouseEvent.MOUSE_CLICKED, mouse);
+
+
+        Scene scene = new Scene(hbox);
+        stage.setScene(scene);
+        stage.setX(200);
+        stage.setY(300);
+        stage.setMinHeight(500);
+        stage.setMinWidth(300);
+        stage.setTitle("Calculator");
+        stage.show();
+
+    }
+
+    public static String cal(String c){
+        String res = "";
+        if(c.contains("+")){
+            res = sum(c);
+        }
+        else if(c.contains("-")){
+            res = minus(c);
+        }
+        if(c.contains("*")){
+            res = multiply(c);
+        }
+        if(c.contains("/")){
+            res = divide(c);
+        }
+        return res;
+    }
+
+    public static String sum(String c) {
+        int secondi = 0;
+        String first = "";
+        String second = "";
+        for (int i = 0; i < c.length(); i++) {
+            int value = c.charAt(i);
+            if (value == 43) {
+                break;
+            }
+            first += c.charAt(i);
+            secondi = i;
+
+        }
+        second = c.substring((secondi+2),c.length() );
+        int f1 = Integer.parseInt(first);
+        int f2 = Integer.parseInt(second);
+        int res = f1 + f2;
+        return res+"";
+
+    }
+
+    public static String minus(String c) {
+        int secondi = 0;
+        String first = "";
+        String second = "";
+        for (int i = 0; i < c.length(); i++) {
+            int value = c.charAt(i);
+            if (value == 45) {
+                break;
+            }
+            first += c.charAt(i);
+            secondi = i;
+
+        }
+        second = c.substring((secondi+2),c.length() );
+        int f1 = Integer.parseInt(first);
+        int f2 = Integer.parseInt(second);
+        int res = f1 - f2;
+        return res+"";
+
+    }
+    public static String multiply(String c) {
+        int secondi = 0;
+        String first = "";
+        String second = "";
+        for (int i = 0; i < c.length(); i++) {
+            int value = c.charAt(i);
+            if (value == 42) {
+                break;
+            }
+            first += c.charAt(i);
+            secondi = i;
+
+        }
+        second = c.substring((secondi+2),c.length() );
+        int f1 = Integer.parseInt(first);
+        int f2 = Integer.parseInt(second);
+        int res = f1 * f2;
+        return res+"";
+
+    }
+
+    public static String divide(String c) {
+        int secondi = 0;
+        String first = "";
+        String second = "";
+        for (int i = 0; i < c.length(); i++) {
+            int value = c.charAt(i);
+            if (value == 47) {
+                break;
+            }
+            first += c.charAt(i);
+            secondi = i;
+
+        }
+        second = c.substring((secondi+2),c.length() );
+        int f1 = Integer.parseInt(first);
+        int f2 = Integer.parseInt(second);
+        int res = f1 / f2;
+        return res+"";
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+
+
+    }
+}
+
