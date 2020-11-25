0db41531fca3a4bcbb56f11c6f5d04cc8fa01019
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-11-24 18:02:24 -0800

    Update Animation.java

--- Assignment7/Animation.java
+++ Assignment7/Animation.java
@@ -1,19 +1,55 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import javafx.event.*;
+import javafx.scene.input.*;
 
-public class Main extends Application {
+
+
+public class Animation extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage stage) throws Exception{
+        GridPane p1 = new GridPane();
+        p1.setStyle("-fx-background-color: #CC2AFE;");
+
+        GridPane p = new GridPane();
+        TextField outputfield = new TextField("0");
+        p.add(outputfield, 0, 0, 1, 1);
+        p.setHgap(10);
+        p.setVgap(10);
+
+        Circle c = new Circle();
+        c.setRadius(30);
+        c.setFill(Color.CYAN);
+
+        c.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event){
+            c.setFill(Color.RED);
+        }
+        });
+
+
+        GridPane gri = new GridPane();
+        gri.setHgap(10);
+        gri.add(c, 0, 0, 1, 1);
+        p1.setStyle("-fx-background-color: #CC2AFE;");
+
+        p1.add(p, 0, 0, 1, 1);
+        p1.add(gri, 0, 1, 1, 1);
+        Scene scene = new Scene(p1);
+        stage.setScene(scene);
+        stage.setMinHeight(300);
+        stage.setMinWidth(400);
+        stage.setTitle("Animation");
+        stage.show();
     }
 
 
@@ -21,3 +57,4 @@ public class Main extends Application {
         launch(args);
     }
 }
+
