bfd4c99666c75cc2e66550cc7bc149a94b6f84e4
Tanu <24976766+tanusgit@users.noreply.github.com>
2020-11-26 17:10:34 -0800

    Update Animation.java

--- Assignment7/Animation.java
+++ Assignment7/Animation.java
@@ -1,9 +1,8 @@
-package sample;
+
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
@@ -22,19 +21,71 @@ public class Animation extends Application {
         GridPane p1 = new GridPane();
         p1.setStyle("-fx-background-color: #CC2AFE;");
 
+        //creating menubar for adding menus
+        MenuBar menubar = new MenuBar();
+
+        //creating menus for transition
+        Menu rotatemenu = new Menu("Rotate");
+        Menu scalemenu = new Menu("Scale");
+        Menu seqmenu = new Menu("Sequential");
+        Menu fademenu = new Menu("Fade");
+        //attaching menus to menubar
+        menubar.getMenus().add(rotatemenu);
+        menubar.getMenus().add(scalemenu);
+        menubar.getMenus().add(seqmenu);
+        menubar.getMenus().add(fademenu);
+
+        //creating submenus for rotatemenu
+        MenuItem boxrotate = new MenuItem("Rotate box");
+        MenuItem sphereRotate = new MenuItem("Rotate sphere");
+        MenuItem cylinderRotate = new MenuItem("Rotate cylinder");
+        MenuItem polygonRotate = new MenuItem("Rotate polygon");
+
+
+        //attaching submenus to the rotatemenu
+        rotatemenu.getItems().add(boxrotate);
+        rotatemenu.getItems().add(sphereRotate);
+        rotatemenu.getItems().add(cylinderRotate);
+        rotatemenu.getItems().add(polygonRotate);
+
+
+        //creating submenus for scalemenu
+        MenuItem boxScale = new MenuItem("Scale box");
+        MenuItem sphereScale = new MenuItem("Scale sphere");
+        MenuItem cylinderScale = new MenuItem("Scale cylinder");
+        MenuItem polygonScale = new MenuItem("Scale polygon");
+        //attaching submenus to the Scalemenu
+        scalemenu.getItems().add(boxScale);
+        scalemenu.getItems().add(sphereScale);
+        scalemenu.getItems().add(cylinderScale);
+        scalemenu.getItems().add(polygonScale);
+
+        //creating submenus for seqmenu
+        MenuItem boxSeq = new MenuItem("Sequential transition for box");
+        MenuItem sphereSeq = new MenuItem("Sequential transition for sphere");
+        MenuItem cylinderSeq = new MenuItem("Sequential transition for cylinder");
+        MenuItem polygonSeq = new MenuItem("Sequential transition for polygon");
+        //attaching submenus to the Seqmenu
+        seqmenu.getItems().add(boxSeq);
+        seqmenu.getItems().add(sphereSeq);
+        seqmenu.getItems().add(cylinderSeq);
+        seqmenu.getItems().add(polygonSeq);
+
+        //creating submenus for fademenu
+        MenuItem polyfade= new MenuItem("Fade transition for polygon");
+        //attaching submenus to the fademenu
+        fademenu.getItems().add(polyfade);
+
+        
         GridPane button = new GridPane();
         button.setHgap(10);
         button.setVgap(10);
         button.setBorder(new Border(new BorderStroke(Color.GREEN, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3))));
 
-        Button rotate = new Button("Rotate");
-        Button scale = new Button("Scale");
-        Button fade = new Button("Fade");
-        Button seq = new Button("Sequential");
-        button.add(rotate, 0, 0, 1, 1);
-        button.add(scale, 0, 1, 1, 1);
-        button.add(fade, 0, 2, 1, 1);
-        button.add(seq, 0, 3, 1, 1);
+
+        //attaching menubar to button grid
+        button.add(menubar, 0, 0, 1, 1);
+
 
         Box b = new Box();
         b.setWidth(80);
@@ -49,7 +100,7 @@ public class Animation extends Application {
                 PhongMaterial ph = new PhongMaterial();
                 ph.setDiffuseColor(Color.CYAN);
                 b.setMaterial(ph);
-                b.setRotate(30);
+               // b.setRotate(30);
             }
         });
 
@@ -127,6 +178,114 @@ public class Animation extends Application {
         p.setBorder(new Border(new BorderStroke(Color.YELLOW, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3))));
         gri.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3))));
 
+
+        //adding event handlers for rotating shapes
+        boxrotate.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                PhongMaterial ph = new PhongMaterial();
+                ph.setDiffuseColor(Color.CYAN);
+                b.setMaterial(ph);
+                b.setRotate(50);
+            }
+        });
+
+        //adding event handlers for rotating shapes
+        sphereRotate.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                PhongMaterial ph = new PhongMaterial();
+                ph.setDiffuseColor(Color.CYAN);
+                s.setMaterial(ph);
+                s.setRotate(50);
+            }
+        });
+
+        //adding event handlers for rotating shapes
+        polygonRotate.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                poly.setRotate(50);
+            }
+        });
+
+        //adding event handlers for rotating shapes
+        cylinderRotate.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                PhongMaterial ph = new PhongMaterial();
+                ph.setDiffuseColor(Color.CYAN);
+                c.setMaterial(ph);
+                c.setRotate(50);
+            }
+        });
+
+
+        //adding event handlers for scaling shapes
+        boxScale.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                PhongMaterial ph = new PhongMaterial();
+                ph.setDiffuseColor(Color.CYAN);
+                b.setMaterial(ph);
+                b.setScaleX(2);
+                b.setScaleY(2);
+                b.setScaleZ(2);
+            }
+        });
+
+        //adding event handlers for scaling shapes
+        sphereScale.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                PhongMaterial ph = new PhongMaterial();
+                ph.setDiffuseColor(Color.RED);
+                s.setMaterial(ph);
+                s.setScaleX(2);
+                s.setScaleY(2);
+                s.setScaleZ(2);
+            }
+        });
+
+        //adding event handlers for scaling shapes
+        polygonScale.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                poly.setFill(Color.DEEPPINK);
+                poly.setScaleX(2);
+                poly.setScaleY(2);
+                poly.setScaleZ(2);
+            }
+        });
+
+        //adding event handlers for scaling shapes
+        cylinderScale.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                PhongMaterial ph = new PhongMaterial();
+                ph.setDiffuseColor(Color.YELLOWGREEN);
+                c.setMaterial(ph);
+                c.setScaleX(2);
+                c.setScaleY(2);
+                c.setScaleZ(2);
+            }
+        });
+
+        //adding event handlers for sequential transition for shapes
+        cylinderSeq.setOnAction( new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event){
+                PhongMaterial ph = new PhongMaterial();
+                ph.setDiffuseColor(Color.YELLOWGREEN);
+                c.setMaterial(ph);
+                
+            }
+        });
+
+
+
+
+
         p1.setStyle("-fx-background-color: #CC2AFE;");
         p1.add(p, 0, 0, 1, 1);
         p1.add(gri, 0, 1, 1, 1);
@@ -144,4 +303,3 @@ public class Animation extends Application {
         launch(args);
     }
 }
-
